deaae3e2eeb577eb179e0da50d6e918f45b91a31
NicoPipishorts <87517338+NicoPipishorts@users.noreply.github.com>
2022-11-16 13:12:06 +0100

    Structure prepared for new branch; Creating React-Redux Branch

--- src/Components/App/index.jsx
+++ src/Components/App/index.jsx
@@ -1,10 +1,7 @@
 // Import NPM
 import { Routes, Route  } from "react-router-dom";
-import { useQuery } from "react-query";
-import { useState } from "react";
 
 // Import Assets
-import { apiGeocoding } from "../../Hooks/apiGeocoding";
 import './styles.scss';
 
 // Import Components
@@ -13,38 +10,21 @@ import Dashboard from '../Dashboard';
 
 const App = () => {
 
-    const [zipcode, setZipcode] = useState();
-    const [cityName, setCitName] = useState();
-
-    const apiKey = "c3b933db36214330f5baa93574803f1d"
-
-    const geocodingByName = `http://api.openweathermap.org/geo/1.0/direct?q=Paris&limit=1&appid=${apiKey}`;
-    const geocodingByZip = `http://api.openweathermap.org/geo/1.0/zip?zip=90275,US&appid=${apiKey}`;
-
-    const {
-        isLoading, 
-        isError, 
-        data: geoCoding, 
-        error } = 
-        useQuery(
-            ['geoCoding'],
-            () => apiGeocoding(geocodingByZip)
-        );
-
-    console.log("This is the city name :", cityName);
-
-    return (
-    <Routes>
-				<Route path="/" element= {
+	return (
+	
+	<main className="app-wrapper">
+		<Routes>
+			<Route path="/" element= {
 					<Dashboard />
-				} />
-				<Route
+			} />
+			<Route
 					path="/about"
 					element= {<About />}
-				/>
-    </Routes>
+			/>
+		</Routes>
+	</main>
 
-    )
+	)
     
 }
 
--- src/Components/Dashboard/index.jsx
+++ src/Components/Dashboard/index.jsx
@@ -1,15 +1,32 @@
 // Import NPM
+import { useState } from "react";
 
 // Import Assets
+import { TextField, Button } from "@mui/joy"
 
 // Import Components
 
+const apiKey = "c3b933db36214330f5baa93574803f1d"
+
 const Dashboard = () => {
 
+  const [zipcode, setZipcode] = useState();
+  const [cityName, setCityName] = useState('London');
+
   return (
-    <main className="app-wrapper">
-      <h1 className="app-title">Welcome to Pipz Weather Dashboard!</h1>
-    </main>
+    <>
+      <h1 className="dashboard-title">Welcome to Pipz Weather Dashboard!</h1>
+      <TextField
+        placeholder="Search by City Name" 
+        onChange={e => setCityName(e.target.value)} 
+      />
+      <br />
+      {/* <TextField
+        placeholder="Search by Zip Code" 
+        onChange={e => setZipcode(e.target.value)} 
+      /> */}
+      <Button variant="outlined" onClick>Search</Button>
+    </>
   )
 
 }
